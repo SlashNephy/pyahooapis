cda2b48f3ed109c7864f95d1375c31d68c8078d4
Nep <spica@starry.blue>
2016-03-31 02:55:01 +0900

    Update: JSON

--- pyahooapis/utils.py
+++ pyahooapis/utils.py
@@ -1,16 +1,7 @@
+#! python3
 #coding: utf-8
 
 def import_json():
-    try:
-        '''Python 2.6 or later'''
-        import json
-        return json
-    except:
-        try:
-            '''simplejson'''
-            import simplejson
-            return simplejson
-        except:
-            '''Google App Engine'''
-            import django.utils.simplejson
-            return django.utils.simplejson
+    import json
+    return json
+
